d4239f6d867f24fcbdc1ddae188f46800ba12505
Unknown <pablo.osorio@xmarts.com>
2018-09-24 09:31:36 -0500

    Add files
    Add files of module

---
+++ monitoring_control/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Monitoring Control for Belenes",
+
+    'summary': """
+        Module to add customizatios for monitorins control""",
+
+    'description': """
+        Module to add customizatios for monitorins control
+    """,
+
+    'author': "Xmarts, Pablo Osorio Gama",
+    'website': "http://xmarts.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','stock','purchase'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'reports/reports.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ monitoring_control/models/models.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+# class modulo(models.Model):
+#     _name = 'modulo.modulo'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+
+class StockPickingAddFields(models.Model):
+	_name="stock.picking"
+	_inherit="stock.picking"
+
+	@api.one
+	def _document_id(self):
+		self.n_document='DOC0'+str(self.id)
+
+	tipo_reg = fields.Selection((('entrada','Entrada'),('salida','Salida')),string="Tipo de registro")
+	n_document = fields.Char(string="N° de documento", compute=_document_id)
+	hora_llegada = fields.Datetime(string="Fecha y hora de llegada")
+	hora_ingreso = fields.Datetime(string="Fecha y hora de ingreso")
+	nombre_chofer = fields.Char(string="Nombre del chofer")
+	tipo_trans = fields.Selection((('1ton','1 Tonelada'),('3ton','3 Toneladas'),('5ton','5 Toneladas'),('10ton','10 Toneladas'),('torton','Torton'),('48pi','48 Pies'),('53pi','53 Pies'),('full','Full'),('c20','Contenedor 20'),('c40','Contenedor 40'),('c48','Contenedor 48'),('c53','Contenedor 53')),string="Tipo de transporte")
+	placas_tractor = fields.Char(string="Placas del tractor")
+	placas_caja = fields.Char(string="Placas de la caja")
+	placas_caja_dos = fields.Char(string="Placas de la caja 2")
+	condiciones_trans = fields.Text(string="Condiciones del transporte")
+	proveedor_linea = fields.Char(string="Proveedor y linea de transporte")
+	tipo_licencia = fields.Selection((('autom','Automovilista'),('chofer','Chofer'),('federal','Federal')), string="Tipo de licencia")
+	licencia = fields.Char(string="N° de licencia")
+	origen = fields.Char(string="Origen")
+	destino = fields.Char(string="Destino")
+	cedis = fields.Char(string="Cedis")
+
+
+
+#class StockPickingType(models.Model):
+#	_name="stock.picking.type"
+#	_inherit="stock.picking.type"
+#
+#	monitoring_control =fields.Boolean(string="Control de vigilancia", default=False)
